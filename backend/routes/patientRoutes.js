31824035fdefd4e7b3f22bb51871d6bd24579dca
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-11-20 21:28:29 -0600

    🔄 add patient routes

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { addPatient, getPatients } from '../controllers/patientController.js';
+import {
+  addPatient, 
+  getPatients,
+  getPatient,
+  editPatient,
+  deletePatient,
+ } from '../controllers/patientController.js';
 import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +13,10 @@ const router = express.Router();
 router.route('/')
   .post(checkAuth, addPatient)
   .get(checkAuth, getPatients);
+
+router.route('/:id')
+  .get(checkAuth, getPatient)
+  .put(checkAuth, editPatient)
+  .delete(checkAuth, deletePatient);
 
 export default router;
