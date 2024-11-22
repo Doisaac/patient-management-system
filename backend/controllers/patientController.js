7b0d979c9b12727283ffb0b9c2ba6bb4964d4e54
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-11-21 22:03:05 -0600

    🌳 edit a patient

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -42,7 +42,37 @@ const getPatient = async (req, res) => {
 };
 
 const editPatient = async (req, res) => {
+  const {id} = req.params;
+  let patient;
 
+  // Gets the patient
+  try {
+    patient = await Patient.findById(id)
+  } catch (error) {
+    return res.status(404).json({ msg: 'The patient was not found' });
+  }
+
+  const veterinarianId = patient.veterinarianID.toString();
+  const veterinarianIdFromRequest = req.veterinarian._id.toString();
+
+  if (veterinarianId !== veterinarianIdFromRequest ) {
+    return res.json({ msg: 'Invalid action' });
+  }
+
+  // Updates patient information
+  patient.name = req.body.name || patient.name;
+  patient.owner = req.body.owner || patient.owner;
+  patient.email = req.body.email || patient.email;
+  patient.dischargeDate = req.body.dischargeDate || patient.dischargeDate;
+  patient.symptoms = req.body.symptoms || patient.symptoms;
+
+  try {
+    const updatedPatient = await patient.save();
+    res.json(updatedPatient);
+  } catch (error) {
+    res.json({ msg: 'There was an error editing the patient' });
+    console.log(error);
+  }
 };
 
 const deletePatient = async (req, res) => {
