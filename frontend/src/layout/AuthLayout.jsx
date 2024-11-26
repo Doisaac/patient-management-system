3cebfe46d75b0bbc4fb89e44a0c93bf3c2cc2a17
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-11-25 22:55:15 -0600

    🥇 add first login ui

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
     <>
-      <h1>Patient Management System</h1>
-      
-      <Outlet />
+      <main className="container mx-auto md:grid md:grid-cols-2 mt-12 gap-10">
+        <Outlet />
+      </main>
     </>
-  )
+  );
 }
 
 export default AuthLayout
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,53 @@
 const Login = () => {
   return (
     <>
-      <h1>Login</h1>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Log in and Manage your 
+          <span className="text-black"> Patients</span>
+        </h1>
+      </div>
+
+      <div>
+        <form action="">
+          <div className="my-5">
+            <label 
+              className="uppercase text-gray-500 block text-xl font-bold"
+            >
+              Email:
+            </label>
+
+            <input 
+              type="email"
+              placeholder="Registration email"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+        
+          <div className="my-5">
+            <label 
+              className="uppercase text-gray-500 block text-xl font-bold"
+            >
+              Password:
+            </label>
+
+            <input 
+              type="password"
+              placeholder="Password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <input 
+            type="submit"
+            value="Login"
+            className="bg-indigo-700 w-full py-3 rounded-xl text-white uppercase font-bold mt-5 cursor-pointer hover:bg-indigo-900 transition-colors ease-in-out duration-700 md:w-auto px-12"
+          />
+
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
 export default Login
