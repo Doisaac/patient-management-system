2e0df220a51d0a4f2f28bb923c2ea04bbf77dfeb
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-11-26 22:56:04 -0600

    ⚾ add register page title

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,12 @@
 const Register = () => {
   return (
     <>
-      <h1>Register</h1>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl text-pretty">
+          Sign Up and Start Managing Your {""}
+          <span className="text-black">Patients</span> 
+        </h1>
+      </div>
     </>
   )
 }
