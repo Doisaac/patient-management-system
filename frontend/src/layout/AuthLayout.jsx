dbc9559344acb3f3ef673620889c989af084913f
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-11-27 19:32:51 -0600

    🖌️ add styles

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
     <>
-      <main className="container mx-auto md:grid md:grid-cols-2 mt-12 gap-10 items-center">
+      <main className="container mx-auto md:grid md:grid-cols-2 gap-10 items-center h-full p-3 md:p-0">
         <Outlet />
       </main>
     </>
