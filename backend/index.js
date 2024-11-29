25af17bd4342e9ba909b35d8b6d9f54ce6c02422
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-11-29 16:38:05 -0600

    🟠 allow to use the API using cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import veterinanRoutes from './routes/veterinarianRoutes.js';
 import patientRoutes from './routes/patientRoutes.js'
 
 const app = express();
 connectDB();
+
+const allowedDomains = ['http://localhost:5173'];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (allowedDomains.indexOf(origin) !== -1){
+      // The origin is allowed
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 // Allows to parse request bodies
 app.use(express.json());
