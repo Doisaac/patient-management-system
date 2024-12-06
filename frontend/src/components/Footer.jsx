75840c8e0af5885cc83bc8177007d428934b0812
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-06 16:31:56 -0600

    🦶 add footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="py-10">
+      <p className="text-center font-bold">
+        PMS - Veterinary Patient Management {''}
+        <span className="text-indigo-600">System</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
