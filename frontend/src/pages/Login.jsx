6b5e8f20d5fa1d98f6e67ad493fbdac2ace73983
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-05 22:14:10 -0600

    🪙 add token to local storage

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,33 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Alert from '../components/Alert.jsx';
+import axiosClient from '../config/axios.jsx';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alert, setAlert] = useState({});
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const fields = [email, password].map(i => i.trim());
+
+    if (fields.includes('')){
+      setAlert({ msg: 'Complete all the fields', error: true});
+      return;
+    }
+
+    try {
+      const { data: {token} } = await axiosClient.post('/veterinarians/login', { email, password});
+
+      // Saves the token
+      localStorage.setItem('token', token);
+    } catch (error) {
+      setAlert({ msg: error.response.data.msg, error: true});
+    }
+  }
+
   return (
     <>
       <div>
@@ -11,7 +38,11 @@ const Login = () => {
       </div>
 
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form action="">
+        {alert.msg && <Alert alertObj={alert}/>}
+
+        <form
+         onSubmit={handleSubmit}
+        >
           <div className="my-5">
             <label className="uppercase text-gray-500 block text-xl font-bold">
               Email:
@@ -21,6 +52,8 @@ const Login = () => {
               type="email"
               placeholder="Registration email"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              onChange={e => setEmail(e.target.value)}
+              value={email}
             />
           </div>
 
@@ -33,6 +66,8 @@ const Login = () => {
               type="password"
               placeholder="Password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              onChange={e => setPassword(e.target.value)}
+              value={password}
             />
           </div>
 
