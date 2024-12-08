67b9768774b96219f837750d659a13e038fd47b6
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-07 19:57:59 -0600

    🏝️ add base design

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,7 +1,17 @@
+import Form from "../components/Form"
+import PatientList from "../components/PatientList"
+
 const Admin = () => {
   return (
     <>
-      <h1>Patient Management</h1>
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-1/2 lg:w-2/5">
+          <Form />
+        </div>
+        <div className="md:w-1/2 lg:w-2/5">
+          <Form />
+        </div>
+      </div>
     </> 
   )
 }
