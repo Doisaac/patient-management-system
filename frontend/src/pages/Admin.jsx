6ea919091cb7f9c74983ff9c1313519926c4963b
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-08 18:09:56 -0600

    ⏹️ add form button

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,19 +1,31 @@
+import { useState } from 'react'
 import Form from "../components/Form"
 import PatientList from "../components/PatientList"
 
 const Admin = () => {
+  const [showForm, setShowForm] = useState();
+
   return (
     <>
       <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/2 lg:w-2/5">
+        <button
+          type="button"
+          className="bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-md md:hidden"
+          onClick={() => {setShowForm(!showForm)}}
+        > 
+          {showForm ? 'Hide Form ' : 'Show Form'}
+        </button>
+        
+        <div className={`${showForm ? 'block' : 'hidden'} md:block md:w-1/2 lg:w-2/5`}>
           <Form />
         </div>
+        
         <div className="md:w-1/2 lg:w-2/5">
           <PatientList />
         </div>
       </div>
-    </> 
-  )
+    </>
+  );
 }
 
 export default Admin
