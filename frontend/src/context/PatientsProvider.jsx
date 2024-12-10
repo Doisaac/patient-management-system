7bc74230ca61bfe8e10c4f64b19cb33e1641dfa0
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-10 15:37:01 -0600

    🥇 get patients from the db

--- frontend/src/context/PatientsProvider.jsx
+++ frontend/src/context/PatientsProvider.jsx
@@ -6,6 +6,28 @@ const PatientsContext = createContext();
 export const PatientsProvider = ({children}) => {
   const [patients, setPatients] = useState([]);
 
+  useEffect(() => {
+    const getPatients = async() => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) return;
+    
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+          }
+        };
+
+        const {data} = await axiosClient('/patients', config);
+        setPatients(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getPatients();
+  }, []);
+
   const savePatient = async (patient) => {
     try {
       const token = localStorage.getItem('token');
