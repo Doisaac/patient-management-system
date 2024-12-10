6ca6dd2aad177aac52e72ec6db8b9693b657b95a
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-10 13:53:03 -0600

    🙀 add patient context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { AuthProvider } from './context/authProvider.jsx'
+import { PatientsProvider } from './context/PatientsProvider.jsx'
 import AuthLayout from './layout/AuthLayout'
 import RestrictedArea from './layout/RestrictedArea.jsx'
 import Login from './pages/Login'
@@ -14,19 +15,21 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout/>}>
-            <Route index element={<Login/>}/>
-            <Route path="register" element={<Register/>}/>
-            <Route path="forget-password" element={<ForgetPassword/>}/>
-            <Route path="forget-password/:token" element={<NewPassword/>}/>
-            <Route path="confirm/:token" element={<ConfirmAccount/>}/>
-          </Route>
+        <PatientsProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout/>}>
+              <Route index element={<Login/>}/>
+              <Route path="register" element={<Register/>}/>
+              <Route path="forget-password" element={<ForgetPassword/>}/>
+              <Route path="forget-password/:token" element={<NewPassword/>}/>
+              <Route path="confirm/:token" element={<ConfirmAccount/>}/>
+            </Route>
 
-          <Route path="/admin" element={<RestrictedArea />}>
-            <Route index element={<Admin/>}/>
-          </Route>
-        </Routes>
+            <Route path="/admin" element={<RestrictedArea />}>
+              <Route index element={<Admin/>}/>
+            </Route>
+          </Routes>
+        </PatientsProvider>
       </AuthProvider>
     </BrowserRouter>
   )
---
+++ frontend/src/context/PatientsProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, useEffect} from 'react'
+import axiosClient from '../config/axios'
+
+const PatientsContext = createContext();
+
+export const PatientsProvider = ({children}) => {
+  return (
+    <PatientsContext.Provider 
+      value={{
+        
+      }}
+    >
+      {children}
+    </PatientsContext.Provider>
+  )
+}
+
+export default PatientsContext
