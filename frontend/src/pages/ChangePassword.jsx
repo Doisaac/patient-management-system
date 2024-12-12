8074f64da8c894f381033c56d1ed5cd87e0d1d7c
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-12 12:12:56 -0600

    📄 add profile pages

---
+++ frontend/src/pages/ChangePassword.jsx
@@ -0,0 +1,20 @@
+import AdminNav from "../components/AdminNav"
+
+const ChangePassword = () => {
+  return (
+    <>
+      <AdminNav />
+
+      <h2 className="font-black text-3xl text-center mt-10">
+        Change Password
+      </h2>
+      
+      <p className="text-xl mt-5 mb-10 text-center">
+        Modify your {''} 
+        <span className="text-indigo-600 font-bold">Password here</span>
+      </p>
+    </>
+  )
+}
+
+export default ChangePassword
