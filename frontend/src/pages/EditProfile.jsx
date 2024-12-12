bfddd460ddbdccf729413c61bca890ebcfc8cb5d
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-12 12:33:26 -0600

    🖌️ add profile edit page design

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -11,6 +11,60 @@ const EditProfile = () => {
         Modify your {''}
         <span className="text-indigo-600 font-bold">Information here</span>
       </p>
+
+      <div className='flex justify-center'>
+        <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
+          <form>
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">
+                Name
+              </label>
+              <input 
+                type='text'
+                className='border bg-gray-50 w-full p-2 mt-5 rounded-lg'
+                name='name'
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">
+                Website
+              </label>
+              <input 
+                type='text'
+                className='border bg-gray-50 w-full p-2 mt-5 rounded-lg'
+                name='web'
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">
+                Phone Number
+              </label>
+              <input 
+                type='text'
+                className='border bg-gray-50 w-full p-2 mt-5 rounded-lg'
+                name='phone-number'
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">
+                Email
+              </label>
+              <input 
+                type='text'
+                className='border bg-gray-50 w-full p-2 mt-5 rounded-lg'
+              />
+            </div>
+
+            <input 
+              type='submit'
+              className='bg-indigo-700 px-10 py-3 font-bold text-white roundedn-lg uppercase w-full mt-5'
+            />
+          </form>
+        </div>
+      </div>
     </>
   );
 };
