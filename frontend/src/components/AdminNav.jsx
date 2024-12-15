c6321c49bfba1048c98718f92105251af14e665d
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-12-15 15:46:00 -0600

    🅰️ add hover effects to the admin navbar

--- frontend/src/components/AdminNav.jsx
+++ frontend/src/components/AdminNav.jsx
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom"
 
 const AdminNav = () => {
   return (
-    <nav className="flex gap-3">
+    <nav className="flex gap-3 justify-center">
       <Link
         to="/admin/profile"
-        className='font-bold uppercase text-gray-500'
+        className='font-bold uppercase text-gray-500 hover:text-indigo-600 transition-colors'
       >
         Profile
       </Link>
 
       <Link
         to="/admin/change-password"
-        className='font-bold uppercase text-gray-500'
+        className='font-bold uppercase text-gray-500 hover:text-indigo-600 transition-colors'
       >
         Change Password
       </Link>
